cce796321a6e1b22f7fa9927623283a4d15cd83d
Norbert Csiki <norbert.csiki.92@gmail.com>
2023-07-25 20:53:32 +0200

    Initial commit

---
+++ src/main/java/com/mbalyi/gallery/GalleryBeApplication.java
@@ -0,0 +1,13 @@
+package com.mbalyi.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryBeApplication.class, args);
+    }
+
+}
